3366c2c9e2f3fa8ff379d3c424269171849d0e6e
Alex <maeleeuxs@gmail.com>
2019-03-12 20:02:26 +0100

    Changes to Welcome component
    Added a random number generator.
    Changed the counter to intervals of 2 and lowered the time-interval to
    500 ms.

--- test-project/src/app/welcome/welcome.component.ts
+++ test-project/src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-welcome',
@@ -7,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
   display: number = 0;
+  random: number;
 
   constructor() {
+    this.newRandom();
+
     setInterval(() => {
-      this.display++;
-    }, 2000);
+      this.display+=2;
+    }, 500);
+
+    setInterval(this.newRandom, 1000);
   }
 
   ngOnInit() {
+  }
+
+  newRandom = () => {
+    this.random = _.random(1,100);
   }
 }
