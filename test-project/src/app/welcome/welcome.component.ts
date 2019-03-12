9fb23e773422ad371c15d7024c8b81184dd84c2c
Alexander <s090133@ap.be>
2019-03-12 19:32:49 +0100

    Creation Welcome component
    This is the first change to our test project

---
+++ test-project/src/app/welcome/welcome.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+  display: number = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.display++;
+    }, 1000);
+  }
+
+  ngOnInit() {
+  }
+}
